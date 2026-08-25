b88284df1af1ad9cdda9554cc526c42de9f70959
Eli Leblanc <eli@spatialnetics.com>
2026-08-25 18:56:06 -0300

    #9 Updated change-guest-password with token

--- api/change-guest-password.js
+++ api/change-guest-password.js
@@ -14,10 +14,9 @@ const AUTH_FILE = process.env.GCLOUD_AUTH_FILE;
 
 export default async function handler(req, res) {
 	// CORS headers
-	res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_ORIGIN);
+	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
-	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
 	if (req.method === "OPTIONS") {
 		return res.status(200).end();
