9b43f3893700c9a5e217373a66142ae713601beb
Eli Leblanc <eli@spatialnetics.com>
2026-08-25 18:02:33 -0300

    Ignore some files

--- generate-password-hash.cjs
+++ generate-password-hash.cjs
@@ -3,7 +3,7 @@
 // 3) Copy the hash from the console
 const bcrypt = require("bcryptjs");
 
-const password = "blabla";
+const password = "Partition";
 bcrypt.hash(password, 12).then((hash) => {
 	console.log(hash);
 });
